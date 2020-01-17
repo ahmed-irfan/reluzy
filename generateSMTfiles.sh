ee75966aa3dd5beb0847ec0e4051c6e7d39d1eb0
Aleksandar Zeljic <zeljic@stanford.edu>
2020-01-16 16:49:37 -0800

    Add simplex-friendly generation to the script.

--- generateSMTfiles.sh
+++ generateSMTfiles.sh
@@ -6,7 +6,9 @@ for nnet in `ls $networks*.nnet`; do
     for prop in 1 2 3 4 5 6; do
        name=`basename $nnet .nnet`_p$prop
        echo $name 
-       python ../nnet2smt.py $nnet property$prop.txt 0 > smt/${name}_e.smt2 
-       python ../nnet2smt.py $nnet property$prop.txt 1 > smt/${name}_l.smt2 
+       python ../nnet2smt.py $nnet property$prop.txt 0 0 > smt/${name}_e.smt2 
+       python ../nnet2smt.py $nnet property$prop.txt 1 0 > smt/${name}_l.smt2 
+       python ../nnet2smt.py $nnet property$prop.txt 0 1 > smt/${name}_e_sf.smt2 
+       python ../nnet2smt.py $nnet property$prop.txt 1 1 > smt/${name}_l_sf.smt2 
     done
 done
